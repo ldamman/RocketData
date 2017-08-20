2d53a4e15849a049ebe1367a525376695584bf57
Larry Damman <ldamman@users.noreply.github.com>
2017-08-20 01:48:30 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 import PackageDescription
 let package = Package(
-  name: "RocketData",
-  targets: [
-    Target(name: "RocketData", dependencies: []),
-  ],
-  dependencies: [
-    .Package(url: "https://github.com/linkedin/ConsistencyManager-iOS.git"),
-  ],
-  exclude: [
+	name: "RocketData",
+	targets: [
+		Target(name: "RocketData", dependencies: []),
+	],
+	dependencies: [
+		.Package(url: "https://github.com/linkedin/ConsistencyManager-iOS.git", majorVersion: 4),
+	],
+	exclude: [
 	]
 )
